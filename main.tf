5734bf071240b509246e24cbf867fdbc45b5e0cc
Rahul Sharma <er.csrahul@gmail.com>
2018-12-28 01:52:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-data "template_file" "basic" {
-  template = "${file("${path.module}/scripts/basic.sh")}"
-}
-
-data "template_file" "fmt" {
-  template = "${file("${path.module}/scripts/fmt.sh")}"
+provider "aws" {
+  region = "us-east-1"
 }
