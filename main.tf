64568259d2795e57b562736d01ca4d5a6fece238
Rahul Sharma <er.csrahul@gmail.com>
2018-12-28 13:39:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
 provider "aws" {
   region = "us-east-1"
 }
-data "template_file" "basic" {
-  template = "${file("${path.module}/scripts/basic.sh")}"
-}
-
-data "template_file" "fmt" {
-  template = "${file("${path.module}/scripts/fmt.sh")}"
-}
